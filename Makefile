b0e562201cf06281ebdbee362f8abe46a8a463a6
Erika Maria Tavera Misas <etavera-@lab2r4s1.42abudhabi.ae>
2023-03-13 15:01:22 +0400

    gnl base

---
+++ Makefile
@@ -0,0 +1,28 @@
+
+SRCS			= get_next_line.c
+OBJS			= $(SRCS:.c=.o)
+
+CC				= gcc
+RM				= rm -f
+CFLAGS			= -Wall -Wextra -Werror
+
+NAME			= libftprintf.a
+
+all:			$(NAME)
+
+$(NAME):		$(OBJS)
+				ar rcs $(NAME) $(OBJS)
+
+clean:
+				$(RM) $(OBJS)
+
+fclean:			clean
+				$(RM) $(NAME)
+
+re:				fclean $(NAME)
+
+.PHONY:			all clean fclean re bonus
+
+
+
+
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 12:02:09 by etavera-          #+#    #+#             */
+/*   Updated: 2023/03/13 13:07:45 by etavera-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+#endif
