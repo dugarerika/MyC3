2c6445e7e25207b9676ecbb38c544d171dcf1600
Erika Maria Tavera Misas <etavera-@lab2r4s1.42abudhabi.ae>
2023-03-15 10:08:16 +0400

    open file

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,31 @@
 /*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 09:32:55 by etavera-          #+#    #+#             */
-/*   Updated: 2023/03/13 13:05:43 by etavera-         ###   ########.fr       */
+/*   Updated: 2023/03/15 10:04:43 by etavera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+int get_next_line(int fd)
 {
+int n;
+char buff[1024];
 
+n = read(fd, buff, 1024);
+if (n == -1)
+	printf("An error occurred while trying to read the file");
+return(n);
+}
+
+# include <fcntl.h>
+int main()
+{
+	// unsigned int a = 0;
+	// printf("%u\n", a);
+	// ft_printf("%u\n", a);
+	int fd;
+	char path[] = "Documents/example.txt";
+	fd = open(path, O_RDONLY);
+	get_next_line(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,20 @@
 /*   By: etavera- <etavera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 12:02:09 by etavera-          #+#    #+#             */
-/*   Updated: 2023/03/13 13:07:45 by etavera-         ###   ########.fr       */
+/*   Updated: 2023/03/15 10:02:11 by etavera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+
+# include <errno.h>
+# include <stdio.h>
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 
-char	*get_next_line(int fd);
+
+int	get_next_line(int fd);
 #endif
